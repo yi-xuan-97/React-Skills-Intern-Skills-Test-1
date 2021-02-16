b8c80d835adcd41e1c33660d48b9aa3915912d36
yi-xuan-97 <56617127+yi-xuan-97@users.noreply.github.com>
2021-02-15 17:44:50 -0800

    basic frame

---
+++ my-app/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+
+      <div className="container">
+
+        <div className="title">
+          <div className="fir_title">CLIMATEHACKS</div>
+          <div className="sec_title">Sat April 24-25 2021</div>
+        </div>
+
+
+
+        <div className="blue_block">
+        </div>
+        <nav>
+          <a href="register">Register</a>
+          <a href="Volunteer">Volunteer</a>
+          <a href="Sponsor">Sponsor</a>
+        </nav>
+
+        <div className="text_part">
+          <h1 className="one">What is Climate Hacks?</h1>
+          <h2 className="two">Organized by student organizers</h2>
+          <br/>
+          <h2 className="three">Who are MinorityProgrammers?</h2>
+        </div>
+
+
+      </div>
+    </body>
+  );
+}
+
+export default App;
